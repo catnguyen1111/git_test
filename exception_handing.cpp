f29cee5709653c6a987765499a1d8b6dbe6b5331
catnguyen1111 <64425871+catnguyen1111@users.noreply.github.com>
2021-11-19 09:42:24 +0700

    fix code

--- exception_handing.cpp
+++ exception_handing.cpp
@@ -1,13 +1,4 @@
 ///////////////////////////////////////////////////Exception Handing /////////////////////////////////////////////////
-/*
-Exception cung cấp một cách để truyền điều khiển từ một phần của một chương trình tới phần khác. Exception Handling (Xử lý ngoại lệ) trong C++ được xây dựng dựa trên 3 từ khóa là: try, catch, và throw.
-
-throw: Một chương trình ném một Exception khi một vấn đề xuất hiện. Việc này được thực hiện bởi sử dụng từ khóa throw trong C++.
-
-catch: Một chương trình bắt một Exception với một Exception Handler tại vị trí trong một chương trình nơi bạn muốn xử lý vấn đề đó. Từ khóa catch trong C++ chỉ dẫn việc bắt một exception.
-
-try: Một khối try có thể được bắt bởi một số lượng cụ thể exception. Nó được theo sau bởi một hoặc nhiều khối catch.
-*/
 #include<iostream>
 #include<math.h>
 #include<stdio.h>
